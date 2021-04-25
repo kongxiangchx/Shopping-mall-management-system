aeb0cf455a98209053976988bac08ac0eec97b8b
kongxiangchx <773046513@qq.com>
2021-04-25 15:07:20 +0800

    传输数据

--- send_data.py
+++ send_data.py
@@ -1,15 +1,18 @@
 import socket
 import json
 
+
 class Send_data:
     def __init__(self):
-        self.s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.s.connect(('127.0.0.1',5000))
-    def message(self,data):
-        dealdata=json.dumps(data)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect(('127.0.0.1', 5000))
+
+    def message(self, data):
+        dealdata = json.dumps(data)
         self.s.send(dealdata.encode())
-        recvdata=self.s.recv(1024)
-        recvdata=recvdata.decode()
+        recvdata = self.s.recv(1024)
+        recvdata = recvdata.decode()
         return json.loads(recvdata)
+
     def close(self):
         self.s.close()
